0b5a06220d5023006a65318a2dd31efea72bd269
Lars Fredrik <lars-on@hotmail.com>
2023-09-05 16:33:26 +0200

    Finished desktop. Project done! 🎉
    Wohoo!

--- Prosjekt 0 - CV/app.js
+++ Prosjekt 0 - CV/app.js
@@ -1,11 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-	let headers = document.querySelectorAll('.resume__entry-header');
-	headers.forEach(header => {
-			header.addEventListener('click', () => {
-					let box = header.nextElementSibling;
-					if (box && box.classList.contains('resume__box--collapsible')) {
-							box.classList.toggle('resume__box--hidden');
-					}
-			});
-	});
+document.addEventListener("DOMContentLoaded", () => {
+  let headers = document.querySelectorAll(".resume__entry-header--collapsible");
+
+  const toggleCollapseAbility = () => {
+    if (window.innerWidth >= 900) {
+      headers.forEach((header) => {
+        header.classList.remove("resume__entry-header--collapsible");
+        header.classList.remove("expanded");
+        header.nextElementSibling.classList.remove("resume__box--hidden");
+        header.removeEventListener("click", toggleBox);
+      });
+    } else {
+      headers.forEach((header) => {
+        header.classList.add("resume__entry-header--collapsible");
+        header.addEventListener("click", toggleBox);
+      });
+    }
+  };
+
+  const toggleBox = (event) => {
+    let box = event.currentTarget.nextElementSibling;
+    event.currentTarget.classList.toggle("expanded");
+    box.classList.toggle("resume__box--hidden");
+  };
+
+  headers.forEach((header) => {
+    header.addEventListener("click", toggleBox);
+  });
+
+  window.addEventListener("resize", toggleCollapseAbility);
+
+  // Initial check to set up the correct state
+  toggleCollapseAbility();
 });
